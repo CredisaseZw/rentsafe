6795c968da5792b8e93376358a6ee872f2a47279
Russellg <dudurussell@gmail.com>
2025-03-20 10:42:45 +0200

    feat: add and map default-invoice-data prop; add trigger customization; implement discount input field; amend submit handler

--- react-app/src/hooks/component-hooks/useSalesInvoiceForm.js
+++ react-app/src/hooks/component-hooks/useSalesInvoiceForm.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-export default function useSalesInvoiceForm() {
+export default function useSalesInvoiceForm(invoice) {
+  const [invoiceData, setInvoiceData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [show, setShow] = useState(false);
   const [currency, setCurrency] = useState("USD");
   const [salesCodes, setSalesCodes] = useState([]);
   const [taxConfigs, setTaxConfigs] = useState([]);
   const [key, setKey] = useState(0);
+  const [discount, setDiscount] = useState(0);
+
   const [items, setItems] = useState([
     {
+      static: false,
       sales_code: "",
       sales_item: "",
       price: "",
@@ -18,6 +23,36 @@ export default function useSalesInvoiceForm() {
       total: "",
     },
   ]);
+
+  useEffect(() => {
+    if (invoice) {
+      const newInvoiceData = {
+        ...invoice,
+
+        document_number: 13652,
+        bill_to: "ASSETSAFE",
+        address: "2 Clip Road, Deven",
+        phone: "+2637856123",
+        email: "assetsafe@gmail.com",
+        vat_no: "124523",
+        tin: "65895",
+        currency: "USD",
+        monthly_rental: {
+          static: true,
+          sales_code: "REN001",
+          sales_item: "Rent - Mar25",
+          price: 500.0,
+          qty: 1,
+          vat_id: "",
+          total: 500.0,
+        },
+      };
+
+      setInvoiceData(newInvoiceData);
+      setCurrency(newInvoiceData.currency);
+      setItems([newInvoiceData.monthly_rental]);
+    }
+  }, [invoice]);
 
   function fetchSalesCodes() {
     axios
@@ -50,6 +85,7 @@ export default function useSalesInvoiceForm() {
     setItems([
       ...items,
       {
+        static: false,
         sales_code: "",
         sales_item: "",
         price: "",
@@ -75,29 +111,26 @@ export default function useSalesInvoiceForm() {
 
   function onSubmit(e) {
     e.preventDefault();
+    if (!items.length) {
+      toast.error("Please add at least one item");
+      return;
+    }
     const data = Object.fromEntries(new FormData(e.target).entries());
-    data.items = items;
+    data.items = items.map((item) => {
+      const newItem = { ...item };
+      newItem.vat =
+        ((parseFloat(newItem.vat) || 0) / 100) *
+        (parseFloat(newItem.price) || 0) *
+        (parseFloat(newItem.qty) || 0);
+      delete newItem.static;
+      return newItem;
+    });
+
+    Object.keys(totals).forEach((key) => (data[key] = totals[key]));
+    data.invoiceTotal += Number(discount);
+    data.discount = Number(discount);
     console.log(data);
   }
-
-  const totals = items.reduce(
-    (acc, item) => {
-      const qty = parseFloat(item.qty) || 0;
-      const unitVat = (parseFloat(item.vat) || 0) / 100;
-      const unitPrice = parseFloat(item.price) || 0;
-      acc.totalExcludingVat += unitPrice * qty;
-      acc.discount += 0;
-      acc.vatTotal += unitPrice * unitVat * qty;
-      acc.invoiceTotal += parseFloat(item.total) || 0;
-      return acc;
-    },
-    {
-      totalExcludingVat: 0,
-      discount: 0,
-      vatTotal: 0,
-      invoiceTotal: 0,
-    }
-  );
 
   function changeCurrency(e) {
     setItems([
@@ -114,21 +147,52 @@ export default function useSalesInvoiceForm() {
     setKey((prev) => prev + 1);
   }
 
+  const totals = items?.reduce(
+    (acc, item) => {
+      if (!item) return acc;
+      const qty = parseFloat(item.qty) || 0;
+      const unitVat = (parseFloat(item.vat) || 0) / 100;
+      const unitPrice = parseFloat(item.price) || 0;
+      acc.totalExcludingVat += unitPrice * qty;
+      acc.vatTotal += unitPrice * unitVat * qty;
+      acc.invoiceTotal += parseFloat(item.total) || 0;
+      return acc;
+    },
+    {
+      totalExcludingVat: 0,
+      vatTotal: 0,
+      invoiceTotal: 0,
+    }
+  );
+
+  function handleDiscount(e) {
+    let value = parseFloat(e.target.value) || 0;
+    if (value > 0) {
+      toast.error("Discount should be input as a negative value");
+    } else {
+      setDiscount(value);
+    }
+  }
+
   return {
     key,
     show,
     items,
     totals,
+    discount,
     currency,
     isLoading,
     salesCodes,
     taxConfigs,
+    invoiceData,
     addRow,
     setItems,
     onSubmit,
     removeRow,
     handleShow,
     handleClose,
+    setDiscount,
     changeCurrency,
+    handleDiscount,
   };
 }
