93d9dfb6f631f5bf088c221d5b07f273e97c513e
godfrey-tankan <godfreytantswakandeya@gmail.com>
2025-05-19 10:06:52 +0200

    Landing page Static files

--- static/Unallocated-b7373b55.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./media/main-52a9d1f2.js";import"es-errors/type";import"es-errors/uri";function e(){return t.jsx("div",{children:"Unallocated"})}export{e as default};
---
+++ static/media/Unallocated-9dd43d32.js
@@ -0,0 +1,1 @@
+import{j as t}from"./main-cde5c441.js";function e(){return t.jsx("div",{children:"Unallocated"})}export{e as default};
