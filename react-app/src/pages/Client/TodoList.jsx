e183cfff8567457902be93b39bb029bf9837cb7a
Russellg <dudurussell@gmail.com>
2025-05-28 10:53:34 +0200

    feat: enhance TodoList layout and general UI (refs #79)

--- react-app/src/pages/Client/TodoList.jsx
+++ react-app/src/pages/Client/TodoList.jsx
@@ -3,6 +3,7 @@ import useToDoList from "../../hooks/page-hooks/useToDoList.js";
 import WorksAndMaintenance from "../../components/Client/ClientView/WorksAndMaintenance.jsx";
 import { formatCurrency } from "../../utils/formatting.js";
 import { truncate } from "lodash";
+import { friendlyDate } from "../../utils/index.js";
 
 export default function TodoList({ status, message, works, reminders, maintenance, Auth }) {
   const {
@@ -35,104 +36,106 @@ export default function TodoList({ status, message, works, reminders, maintenanc
         />
       )}
 
-      <div>
-        <h5 className="text-center bg-danger p-3 mb-0 text-white bg-info">To Do List</h5>
+      <h5 className="text-center bg-danger p-2 m-0 text-white custom-rounded-1">To Do List</h5>
 
-        <div className="custom-bg-grey-2 text-white text-center px-3 py-2 d-flex justify-content-between align-items-center">
-          <div>{new Date().toDateString()}</div>
-          <div>User: {username}</div>
-        </div>
-
-        <table className="table table-responsive table-bordered table-sm">
-          <thead className="sticky-top bg-white shadow-sm">
-            <tr>
-              <th className="text-nowrap">#</th>
-              <th className="text-nowrap custom-mn-w-07">Date</th>
-              <th className="text-nowrap">Function</th>
-              <th className="text-nowrap custom-mn-w-3">Details</th>
-              <th className="text-nowrap">Balance Owing</th>
-              <th className="text-nowrap custom-mn-w-06"></th>
-              <th className="text-nowrap custom-mn-w-06"></th>
-              <th className="text-nowrap custom-mn-w-06"></th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {todos.map((todo, index) => (
-              <tr key={index} className={todo.status === "DONE" ? "c-done-block" : ""}>
-                <th className="text-nowrap">{index + 1}</th>
-
-                <td>{todo.date ? new Date(todo.date).toISOString().split("T")[0] : "N/A"}</td>
-
-                {todo.function.toLowerCase() === "works" ? (
-                  <td
-                    onClick={() => openScheduledWorks(todo)}
-                    className={`text-white bg-primary text-center ${todo.function === "works" ? "c-pointer" : ""}`}
-                  >
-                    {todo.function}
-                  </td>
-                ) : (
-                  <th className="text-nowrap text-center bg-light">{todo.function}</th>
-                )}
-
-                <th className="custom-w-5">
-                  <p className={"m-0 c-line-clamp-1 " + (todo.status === "DONE" ? "c-done" : "")}>
-                    <span className="text-capitalize">{todo?.title}</span>
-                    {todo?.title && todo?.details ? " - " : ""}
-                    <span className="text-capitalize"> {todo?.details}</span>
-                  </p>
-                </th>
-
-                <td
-                  style={{
-                    backgroundColor: todo.color ? todo.color : "inherit",
-                    color: todo.color ? "white" : "inherit",
-                  }}
-                  className="text-center d-block"
-                >
-                  <div>
-                    {todo.balance_owing
-                      ? todo.balance_owing < 0
-                        ? `(${formatCurrency(todo.balance_owing * -1)})`
-                        : formatCurrency(todo.balance_owing)
-                      : "_"}
-                  </div>
-                </td>
-
-                <td
-                  onClick={() => goToOrigin(todo)}
-                  className="bg-info text-white text-center c-pointer"
-                >
-                  View
-                </td>
-
-                <td
-                  className={
-                    "text-white text-center " +
-                    (todo.status === "DONE" ? "c-muted c-not-allowed" : "bg-success c-pointer")
-                  }
-                  onClick={todo.status === "DONE" ? undefined : () => done(todo)}
-                >
-                  Done
-                </td>
-
-                <td
-                  className="text-white text-center bg-danger c-pointer"
-                  onClick={() => dismiss(todo)}
-                >
-                  Dismiss
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-
-        {!Boolean(todos.length) && (
-          <div className="custom-h-2 d-flex justify-content-center align-items-center border border-2">
-            Nothing to show
-          </div>
-        )}
+      <div className="p-3 fw-bold d-flex justify-content-between align-items-center">
+        <div>{friendlyDate(new Date())}</div>
+        <div>{username}</div>
       </div>
+
+      <table className="table table-responsive table-bordered table-sm bg-white">
+        <thead className="position-sticky bg-white shadow-sm c-table-top text-nowrap">
+          <tr>
+            <th>#</th>
+            <th>Date</th>
+            <th>Function</th>
+            <th className="custom-mn-w-5">Details</th>
+            <th>Balance Owing</th>
+            <th className="custom-mn-w-06"></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {todos.map((todo, index) => (
+            <tr key={index} className={todo.status === "DONE" ? "c-done-block" : ""}>
+              <th className="text-nowrap ps-3">{index + 1}</th>
+
+              <td className="ps-3">
+                {todo.date ? new Date(todo.date).toISOString().split("T")[0] : "N/A"}
+              </td>
+
+              {todo.function.toLowerCase() === "works" ? (
+                <td
+                  onClick={() => openScheduledWorks(todo)}
+                  className={`text-white bg-primary text-center ${todo.function === "works" ? "c-pointer" : ""}`}
+                >
+                  {todo.function}
+                </td>
+              ) : (
+                <th className="text-nowrap text-center bg-light">{todo.function}</th>
+              )}
+
+              <th className="custom-w-5  px-3">
+                <p className={"m-0 c-line-clamp-1" + (todo.status === "DONE" ? "c-done" : "")}>
+                  <span className="text-capitalize">{todo?.title}</span>
+                  {todo?.title && todo?.details ? " - " : ""}
+                  <span className="text-capitalize"> {todo?.details}</span>
+                </p>
+              </th>
+
+              <td
+                style={{
+                  backgroundColor: todo.color ? todo.color : "inherit",
+                  color: todo.color ? "white" : "inherit",
+                }}
+                className="text-center d-block"
+              >
+                <div>
+                  {todo.balance_owing
+                    ? todo.balance_owing < 0
+                      ? `(${formatCurrency(todo.balance_owing * -1)})`
+                      : formatCurrency(todo.balance_owing)
+                    : "_"}
+                </div>
+              </td>
+
+              <td>
+                <div className="d-flex justify-content-center align-items-center gap-2">
+                  <button
+                    onClick={() => goToOrigin(todo)}
+                    className="btn flex-fill justify-content-center btn-info text-white"
+                  >
+                    View
+                  </button>
+
+                  <button
+                    disabled={todo.status === "DONE"}
+                    className={"btn btn-success flex-fill justify-content-center "}
+                    onClick={() => done(todo)}
+                  >
+                    Done
+                  </button>
+
+                  <button
+                    className="btn btn-danger flex-fill justify-content-center"
+                    onClick={() => dismiss(todo)}
+                  >
+                    Dismiss
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+
+          {!Boolean(todos.length) && (
+            <tr>
+              <td colSpan={6} className="p-5 text-center">
+                Nothing to show
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </>
   );
 }
