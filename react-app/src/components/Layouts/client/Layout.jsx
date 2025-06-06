d919fbeee5d160589c10167239a8db4dc523cd59
Russellg <dudurussell@gmail.com>
2025-06-06 11:27:13 +0200

    feat: refine structure and styling of root client layout

--- react-app/src/components/Layouts/client/Layout.jsx
+++ react-app/src/components/Layouts/client/Layout.jsx
@@ -1,34 +1,20 @@
 import ClientSidebar from "../../ClientSidebar.jsx";
-import React, { useState } from "react";
-import { Head, Link, usePage } from "@inertiajs/inertia-react";
 import CustomToaster from "../../CustomToaster.jsx";
+import { Head, Link, usePage } from "@inertiajs/inertia-react";
 
-const Layout = ({ children, title }) => {
+export default function Layout({ children, title }) {
   const { Auth } = usePage().props;
-  const [toggle, setToggle] = useState(false);
-
-  const handleClick = () => setToggle((prev) => !prev);
-
-  if (toggle) {
-    document.body.classList.add("drawer-toggled");
-  } else {
-    document.body.classList.remove("drawer-toggled");
-  }
+  const toggleSidebar = () => document.body.classList.toggle("drawer-toggled");
 
   return (
     <>
+      <Head title={title} />
       <CustomToaster />
 
-      <div className="nav-fixed bg-light bar">
-        <Head title={title} />
-
+      <div className="nav-fixed">
         <nav className="top-app-bar navbar navbar-expand bg-info navbar-dark shadow-sm">
           <div className="container-fluid px-4">
-            <button
-              id="drawerToggle"
-              onClick={handleClick}
-              className="btn btn-lg btn-icon order-1 order-lg-0"
-            >
+            <button id="drawerToggle" onClick={toggleSidebar} className="btn rounded-circle">
               <i className="material-icons">menu</i>
             </button>
 
@@ -38,59 +24,36 @@ const Layout = ({ children, title }) => {
               </div>
             </Link>
 
-            <div className="d-flex align-items-center mx-3 me-lg-0">
-              <ul className="navbar-nav d-none d-lg-flex">
-                <li className="nav-item">
-                  <Link className="nav-link" href="#">
-                    {Auth?.company.company_name ? Auth.company.company_name : ""}
-                  </Link>
-                </li>
-              </ul>
-              <div className="d-flex">
-                <div className="dropdown">
-                  <button
-                    className="btn btn-lg btn-icon dropdown-toggle
-                              mdc-ripple-upgraded"
-                    id="dropdownMenuProfile"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    <i className="material-icons">person</i>
-                  </button>
-                  <ul
-                    className="dropdown-menu dropdown-menu-end mt-3"
-                    aria-labelledby="dropdownMenuProfile"
-                  >
-                    <li>
-                      <a className="dropdown-item mdc-ripple-upgraded" href="/client-profile">
-                        <i className="material-icons leading-icon">person</i>
-                        <div className="me-3">Profile</div>
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item mdc-ripple-upgraded" href="/update-password">
-                        <i className="material-icons leading-icon">settings</i>
-                        <div className="me-3">Settings</div>
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item mdc-ripple-upgraded" href="#!">
-                        <i className="material-icons leading-icon">help</i>
-                        <div className="me-3">Help</div>
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item mdc-ripple-upgraded" href={reverseUrl("logout")}>
-                        <i className="material-icons leading-icon">logout</i>
-                        <div className="me-3">Logout</div>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+            <div className="d-flex align-items-center gap-3">
+              <div className="text-white text-decoration-none fw-semibold">
+                {Auth?.company.company_name ? Auth.company.company_name : ""}
+              </div>
+
+              <div className="dropdown">
+                <button
+                  className="btn rounded-circle dropdown-toggle"
+                  id="dropdownMenuProfile"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="c-scale-down material-icons">person</i>
+                </button>
+
+                <ul
+                  className="dropdown-menu dropdown-menu-end border border-2 p-1 mt-3"
+                  aria-labelledby="dropdownMenuProfile"
+                >
+                  <li>
+                    <a
+                      className="dropdown-item h-auto py-1 rounded-3 px-3"
+                      href={reverseUrl("logout")}
+                    >
+                      Logout
+                      <i className="material-icons c-scale-down ms-2">logout</i>
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
@@ -100,27 +63,25 @@ const Layout = ({ children, title }) => {
           <ClientSidebar />
 
           <div id="layoutDrawer_content">
-            <div className="p-4 container-xl">{children}</div>
+            <div className="p-4 container-xxl">{children}</div>
 
-            <footer id="footer" className="py-4 border-top" style={{ minHeight: "74px" }}>
-              <div className="container-xl px-5">
-                <div className="d-flex flex-column flex-sm-row align-items-center justify-content-sm-between small">
-                  <div className="me-sm-2">Copyright © Credit Safe 2023</div>
-                  <div className="d-flex ms-sm-2">
-                    <a className="text-decoration-none" href="#!">
-                      Privacy Policy
-                    </a>
-                    <div className="mx-1">·</div>
-                    <a className="text-decoration-none" href="#!"></a>
-                  </div>
-                </div>
-              </div>
+            <footer
+              id="footer"
+              className="p-4 border-top d-flex justify-content-between align-items-center"
+            >
+              <p className="text-muted small m-0">© Credit Safe 2023</p>
+
+              <button
+                onClick={() => window.scrollTo(0, 0)}
+                type="button"
+                className="btn border rounded-circle p-1 btn-sm"
+              >
+                <i className="material-icons">arrow_upward</i>
+              </button>
             </footer>
           </div>
         </div>
       </div>
     </>
   );
-};
-
-export default Layout;
+}
