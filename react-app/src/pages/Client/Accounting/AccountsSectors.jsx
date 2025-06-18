783f81cd2eac3b9808682d933ed201bb7c1997dd
Russellg <dudurussell@gmail.com>
2025-06-18 16:34:31 +0200

    feat, fix: use standardized CustomTable component for accounts-sectors list; map data items correctly

--- react-app/src/pages/Client/Accounting/AccountsSectors.jsx
+++ react-app/src/pages/Client/Accounting/AccountsSectors.jsx
@@ -1,39 +1,31 @@
-import Layout from "../../../components/Layouts/client/Layout.jsx";
-import ClientSidebarNavItemModal from "../../../components/ClientSidebarNavItemModal.jsx";
 import useAccountsSectors from "../../../hooks/page-hooks/useAccountsSectors.js";
-import NewPageHeader from "../../../components/NewPageHeader.jsx";
+import CustomTable from "../../../components/Client/table/CustomTable.jsx";
+import Layout from "../../../components/Layouts/client/Layout.jsx";
 
 export default function AccountsSectors() {
   const { accountsSectors } = useAccountsSectors();
-
   return (
     <div>
-      <NewPageHeader title="Accounts Sectors" />
-
-      <table className="table bg-white table-responsive table-sm table-bordered">
-        <thead className="sticky-top bg-white shadow-sm">
+      <CustomTable.Table tabletitle="Accounts Sectors" tabletitleBg="info" tabletitleColor="white">
+        <CustomTable.ColGroup ratios={[1, null]} />
+        <thead className={CustomTable.STICKY_TABLE_HEADER_CLASS}>
           <tr>
             <th>Code</th>
             <th>Sector</th>
           </tr>
         </thead>
+
         <tbody>
           {accountsSectors.map((sector, index) => (
             <tr key={index}>
               <td>{sector.code}</td>
-              <td>{sector.sector}</td>
+              <td>{sector.name}</td>
             </tr>
           ))}
 
-          {accountsSectors.length === 0 && (
-            <tr>
-              <td colSpan={2} className="text-center p-5">
-                Nothing to show
-              </td>
-            </tr>
-          )}
+          {accountsSectors.length === 0 && <CustomTable.NothingToShow colSpan={2} />}
         </tbody>
-      </table>
+      </CustomTable.Table>
     </div>
   );
 }
