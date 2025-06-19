6387e579fc90fd0f9a018b3083a91eb6e83ceedb
Russellg <dudurussell@gmail.com>
2025-06-19 16:00:12 +0200

    feat: use standardized CustomTable component in creditor invoices

--- react-app/src/pages/Client/Accounting/CreditorInvoice.jsx
+++ react-app/src/pages/Client/Accounting/CreditorInvoice.jsx
@@ -1,10 +1,10 @@
-import { friendlyDate } from "../../../utils/index.js";
-import SearchBar from "../../../components/SearchBar.jsx";
 import Layout from "../../../components/Layouts/client/Layout.jsx";
-import useCreditorInvoice from "../../../hooks/page-hooks/useCreditorInvoice.js";
+import SearchBar from "../../../components/SearchBar.jsx";
+import CustomTable from "../../../components/Client/table/CustomTable.jsx";
 import ContentModal from "../../../components/ContentModal.jsx";
+import useCreditorInvoice from "../../../hooks/page-hooks/useCreditorInvoice.js";
 import DisbursementsAsyncSelect from "../../../components/DisbursementsAsyncSelect.jsx";
-import NewPageHeader from "../../../components/NewPageHeader.jsx";
+import { friendlyDate } from "../../../utils/index.js";
 
 export default function CreditorInvoice() {
   const {
@@ -29,20 +29,18 @@ export default function CreditorInvoice() {
     <div>
       <ContentModal
         size="xl"
-        titleOverideContent={
-          <div className="bg-danger mb-4 text-white p-1 rounded-2 text-center">
-            Creditor Invoice
-          </div>
-        }
+        title="Creditor Invoice"
         show={showAddModal}
         handleClose={handleClose}
         centerTitle
       >
         <form onSubmit={handleSubmit}>
-          <table className="table table-sm table-bordered table-responsive bg-white">
-            <thead className="position-sticky c-table-top shadow-sm c-z-5">
+          <CustomTable.Table>
+            <CustomTable.ColGroup ratios={[1, null, null, 1, null, 1, 1, 1, 1, 1, 1]} />
+
+            <thead className={CustomTable.STICKY_TABLE_HEADER_CLASS}>
               <tr>
-                <th className="ps-3">Date</th>
+                <th>Date</th>
                 <th>Creditor </th>
                 <th>Details</th>
                 <th>Inv Ref</th>
@@ -184,155 +182,137 @@ export default function CreditorInvoice() {
                   </td>
 
                   <td>
-                    <button
+                    <CustomTable.RemoveRowButtonTemplate
                       disabled={rows.length === 1}
-                      type="button"
-                      className="btn btn-dark btn-sm"
                       onClick={() => removeRow(index)}
-                    >
-                      <i className="material-icons">close</i>
-                    </button>
+                    />
                   </td>
                 </tr>
               ))}
+
               <tr>
-                <td colSpan={10}>
-                  <button type="button" className="btn btn-outline-info btn-sm" onClick={addRow}>
-                    <i className="material-icons me-2">add</i>Add Row
-                  </button>
+                <td colSpan={11} className="text-end">
+                  <CustomTable.AddRowButtonTemplate onClick={addRow} />
                 </td>
               </tr>
             </tbody>
-          </table>
+          </CustomTable.Table>
 
           <div className="text-center mt-3">
-            <button type="submit" className="btn btn-info text-white">
+            <CustomTable.ActionButtonTemplate type="submit" variant="info">
               Submit
-            </button>
+            </CustomTable.ActionButtonTemplate>
           </div>
         </form>
       </ContentModal>
 
-      <NewPageHeader title="Creditor Invoice" color="danger" />
+      <CustomTable.Table
+        tabletitle="Creditor Invoice"
+        tabletitleBg="danger"
+        tabletitleColor="white"
+      >
+        <CustomTable.ColGroup ratios={[1, null, 1, null, 1, 1, 1, 1]} />
 
-      <div className="d-flex justify-content-between align-items-center gap-4 mb-5">
-        <form onSubmit={handleFilters} className="d-flex border  gap-2 align-items-center">
-          <select
-            className="form-select"
-            name="year"
-            id="year"
-            required
-            value={new Date().getFullYear()}
-            onChange={(e) => handleChange(e, index)}
-          >
-            {new Array(new Date().getFullYear() - 2020).fill(0).map((_, i) => (
-              <option key={i} value={new Date().getFullYear() - i}>
-                {new Date().getFullYear() - i}
-              </option>
-            ))}
-          </select>
+        <thead className={CustomTable.STICKY_TABLE_HEADER_CLASS}>
+          <tr>
+            <td colSpan={8}>
+              <div className="pt-1 d-flex justify-content-between align-items-center gap-4">
+                <form onSubmit={handleFilters} className="d-flex gap-2 align-items-center">
+                  <select
+                    className="form-select form-select-sm custom-mn-w-1 custom-mx-w-1"
+                    name="year"
+                    id="year"
+                    required
+                    value={new Date().getFullYear()}
+                    onChange={(e) => handleChange(e, index)}
+                  >
+                    {new Array(new Date().getFullYear() - 2020).fill(0).map((_, i) => (
+                      <option key={i} value={new Date().getFullYear() - i}>
+                        {new Date().getFullYear() - i}
+                      </option>
+                    ))}
+                  </select>
 
-          <select
-            className="form-select"
-            name="month"
-            id="month"
-            required
-            value={(new Date().getMonth() + 1).toString()}
-            onChange={(e) => handleChange(e, index)}
-          >
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-          </select>
+                  <select
+                    className="form-select form-select-sm custom-mn-w-1 custom-mx-w-1"
+                    name="month"
+                    id="month"
+                    required
+                    value={(new Date().getMonth() + 1).toString()}
+                    onChange={(e) => handleChange(e, index)}
+                  >
+                    <option value="1">January</option>
+                    <option value="2">February</option>
+                    <option value="3">March</option>
+                    <option value="4">April</option>
+                    <option value="5">May</option>
+                    <option value="6">June</option>
+                    <option value="7">July</option>
+                    <option value="8">August</option>
+                    <option value="9">September</option>
+                    <option value="10">October</option>
+                    <option value="11">November</option>
+                    <option value="12">December</option>
+                  </select>
+                </form>
 
-          <div>
-            <button type="submit" className="btn btn-success ">
-              Submit
-            </button>
-          </div>
-        </form>
+                <div className="d-flex align-items-center gap-2">
+                  <SearchBar placeholder="Search..." searchBy="q" />
 
-        <div className="custom-mx-w-4">
-          <SearchBar placeholder="Search..." searchBy="q" />
-        </div>
-      </div>
+                  <CustomTable.ActionButtonTemplate
+                    variant="danger"
+                    icon="add"
+                    onClick={openAddModal}
+                  >
+                    New
+                  </CustomTable.ActionButtonTemplate>
+                </div>
+              </div>
+            </td>
+          </tr>
 
-      <div>
-        <h5 className="position-relative text-center mb-2 p-2 mb-0">
-          Creditor Invoice List
-          <div className="position-absolute top-0 end-0">
-            <button className="btn btn-danger" onClick={openAddModal}>
-              <i className="material-icons me-2">add</i>New
-            </button>
-          </div>
-        </h5>
+          <tr>
+            <th>Inv. Date</th>
+            <th>Supplier</th>
+            <th>Inv. Number</th>
+            <th>Details</th>
+            <th>Currency</th>
+            <th className="text-end">Net Amount</th>
+            <th className="text-end">VAT</th>
+            <th className="text-end">Total Amount</th>
+          </tr>
+        </thead>
 
-        <table className="table table-sm table-bordered table-responsive bg-white">
-          <thead className="position-sticky c-table-top text-white bg-danger shadow-sm c-z-5">
-            <tr>
-              <th className="ps-3">Inv. Date</th>
-              <th>Supplier</th>
-              <th>Inv. Number</th>
-              <th>Details</th>
-              <th>Currency</th>
-              <th className="text-end">Net Amount</th>
-              <th className="text-end">VAT</th>
-              <th className="text-end">Total Amount</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {loading ? (
-              <tr>
-                <td colSpan={8}>
-                  <div className="custom-h-3 bg-white d-flex justify-content-center align-items-center">
-                    <div className="spinner-border text-info" role="status">
-                      <span className="visually-hidden">Loading...</span>
-                    </div>
-                  </div>
+        <tbody>
+          {loading ? (
+            <CustomTable.LoadingIndicator colSpan={8} />
+          ) : !Boolean(creditorInvoices?.length) ? (
+            <CustomTable.NothingToShow colSpan={8} />
+          ) : (
+            creditorInvoices?.map((invoice, index) => (
+              <tr key={index}>
+                <td className="text-nowrap">
+                  {invoice.date_created && friendlyDate(invoice.date_created)}
                 </td>
+
+                <td>{invoice.supplier}</td>
+
+                <td className="text-nowrap">{invoice.invoice_number}</td>
+
+                <td>{invoice.details}</td>
+
+                <td>{invoice.currency}</td>
+
+                <td className="text-end">{invoice.net_amount}</td>
+
+                <td className="text-end">{invoice.vat}</td>
+
+                <td className="text-end">{invoice.total_amount}</td>
               </tr>
-            ) : !Boolean(creditorInvoices?.length) ? (
-              <tr>
-                <td colSpan={8}>
-                  <div className="custom-h-2 bg-white d-flex justify-content-center align-items-center">
-                    Nothing to show
-                  </div>
-                </td>
-              </tr>
-            ) : (
-              creditorInvoices?.map((invoice, index) => (
-                <tr key={index}>
-                  <td>{invoice.date_created && friendlyDate(invoice.date_created)}</td>
-
-                  <td className="ps-3">{invoice.supplier}</td>
-
-                  <td className="ps-3">{invoice.invoice_number}</td>
-
-                  <td className="ps-3">{invoice.details}</td>
-
-                  <td className="ps-3">{invoice.currency}</td>
-
-                  <td className="ps-3 text-end">{invoice.net_amount}</td>
-
-                  <td className="ps-3 text-end">{invoice.vat}</td>
-
-                  <td className="ps-3 text-end">{invoice.total_amount}</td>
-                </tr>
-              ))
-            )}
-          </tbody>
-        </table>
-      </div>
+            ))
+          )}
+        </tbody>
+      </CustomTable.Table>
     </div>
   );
 }
