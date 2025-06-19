4eb970631b511799ee101ee456efaad8a1e59867
Russellg <dudurussell@gmail.com>
2025-06-19 11:57:48 +0200

    feat, fix: use standardized CustomTable component for vat settings; correct page title

--- react-app/src/pages/Client/Accounting/Sales/VatSettings.jsx
+++ react-app/src/pages/Client/Accounting/Sales/VatSettings.jsx
@@ -1,4 +1,5 @@
 import Layout from "../../../../components/Layouts/client/Layout.jsx";
+import CustomTable from "../../../../components/Client/table/CustomTable.jsx";
 import useVatSettings from "../../../../hooks/page-hooks/useVatSettings.js";
 
 export default function VatSettings({}) {
@@ -17,31 +18,34 @@ export default function VatSettings({}) {
     <main>
       <form className="row" onSubmit={handleSubmit}>
         <div className="col col-3 py-0">
-          <div className="bg-white border custom-rounded-2 shadow-sm p-4">
-            <div className="mb-5 fw-bold">Registration</div>
+          <div className="bg-white border custom-rounded-1 shadow-sm p-3">
+            <div className="mb-3 fw-bold text-center">Registration</div>
 
-            <div className="form-check mb-4">
+            <div className="form-check mb-3 justify-content-center d-flex align-self-center gap-2">
               <input
-                className="form-check-input"
+                className="form-check-input "
                 disabled
                 // name="vat_registered"
                 type="checkbox"
                 value="registered"
                 id="vat_registered"
               />
-              <label className="form-check-label" htmlFor="vat_registered">
+              <label className="form-check-label small" htmlFor="vat_registered">
                 V.A.T Registered
               </label>
             </div>
 
-            <div className="mb-3">
-              <label htmlFor="vat_registration_number" className="form-label">
-                V.A.T Registration Number
+            <div>
+              <label
+                htmlFor="vat_registration_number"
+                className="form-label d-block text-center small"
+              >
+                VAT Registration Number
               </label>
               <input
                 disabled
                 type="text"
-                className="form-control border"
+                className="form-control form-control-sm border"
                 // name="vat_registration_number"
                 id="vat_registration_number"
               />
@@ -51,127 +55,104 @@ export default function VatSettings({}) {
 
         <div className="col col-9">
           <div>
-            <div className="p-2 fw-bold text-center">Tax Options</div>
+            <CustomTable.Table tabletitle="Tax Options">
+              <CustomTable.ColGroup ratios={[null, null, 1]} />
 
-            <div className="custom-mn-h-2 mb-3">
-              <table className="table table-sm table-respnsive shadow-sm bg-white">
-                <thead className="position-sticky c-table-top c-bg-light shadow-sm c-z-5">
-                  <tr className="c-force-borders text-center">
-                    <th className="ps-3 text-start" style={{ width: "60%" }}>
-                      <div> Description</div>
-                    </th>
-                    <th className="text-start">
-                      <div>Rate (%) </div>
-                    </th>
-                    <th>
-                      <div> </div>
-                    </th>
+              <thead className={CustomTable.STICKY_TABLE_HEADER_CLASS}>
+                <tr>
+                  <th>Description</th>
+                  <th>Rate (%)</th>
+                  <th />
+                </tr>
+              </thead>
+
+              <tbody>
+                {!Boolean(taxOptions?.length || newTaxOptions?.length) && (
+                  <CustomTable.NothingToShow colSpan={3} />
+                )}
+
+                {taxOptions?.map((option, index) => (
+                  <tr key={index}>
+                    <td>{option.description}</td>
+                    <td>{option.rate} %</td>
+
+                    <td>
+                      <CustomTable.RemoveRowButtonTemplate
+                        onClick={() => removeTaxOption(option)}
+                      />
+                    </td>
                   </tr>
-                </thead>
+                ))}
 
-                <tbody>
-                  {!Boolean(taxOptions?.length || newTaxOptions?.length) && (
-                    <tr>
-                      <td colSpan={3}>
-                        <div className="p-5 bg-white d-flex justify-content-center align-items-center">
-                          Nothing to show
-                        </div>
-                      </td>
-                    </tr>
-                  )}
+                {newTaxOptions?.map((option, index) => (
+                  <tr key={index}>
+                    <td>
+                      <div>
+                        <input
+                          type="text"
+                          className="form-control form-control-sm"
+                          placeholder="description"
+                          name={"description-" + index}
+                          id={"description-" + index}
+                          value={option.description}
+                          required
+                          onChange={(e) => changeHandler("description", e.target.value, option.id)}
+                        />
+                      </div>
+                    </td>
 
-                  {taxOptions?.map((option, index) => (
-                    <tr key={index}>
-                      <td className="ps-3">{option.description}</td>
+                    <td>
+                      <div>
+                        <input
+                          type="number"
+                          step={0.001}
+                          className="form-control form-control-sm"
+                          placeholder="rate in %"
+                          name={"rate-" + index}
+                          required
+                          id={"rate-" + index}
+                          value={option.rate}
+                          onChange={(e) => changeHandler("rate", e.target.value, option.id)}
+                        />
+                      </div>
+                    </td>
 
-                      <td className="ps-3">{option.rate} %</td>
+                    <td>
+                      <CustomTable.RemoveRowButtonTemplate
+                        onClick={() => removeNewTaxOption(option.id)}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </CustomTable.Table>
 
-                      <td className="pe-3 custom-mx-w-05 text-end">
-                        <button
-                          type="button"
-                          className="btn p-1 btn-sm btn-danger"
-                          onClick={() => removeTaxOption(option)}
-                        >
-                          <i className="material-icons">close</i>
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-
-                  {newTaxOptions?.map((option, index) => (
-                    <tr key={index}>
-                      <td className="ps-3">
-                        <div>
-                          <input
-                            type="text"
-                            className="form-control"
-                            placeholder="description"
-                            name={"description-" + index}
-                            id={"description-" + index}
-                            value={option.description}
-                            required
-                            onChange={(e) =>
-                              changeHandler("description", e.target.value, option.id)
-                            }
-                          />
-                        </div>
-                      </td>
-
-                      <td className="ps-3">
-                        <div>
-                          <input
-                            type="number"
-                            step={0.001}
-                            className="form-control"
-                            placeholder="rate in %"
-                            name={"rate-" + index}
-                            required
-                            id={"rate-" + index}
-                            value={option.rate}
-                            onChange={(e) => changeHandler("rate", e.target.value, option.id)}
-                          />
-                        </div>
-                      </td>
-
-                      <td className="pe-3 custom-mx-w-05 text-end">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-danger"
-                          onClick={() => removeNewTaxOption(option.id)}
-                        >
-                          <i className="material-icons">close</i>
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-
-            <div className="text-end">
-              <button
-                type="button"
+            <div className="text-end mt-3">
+              <CustomTable.ActionButtonTemplate
                 disabled={loading}
-                className="btn btn-outline-info "
                 onClick={addNewTaxOption}
+                icon="add"
               >
-                <i className="leading-icon material-icons">add</i>
                 Add Tax Item
-              </button>
+              </CustomTable.ActionButtonTemplate>
             </div>
 
-            <div className="text-center">
-              <button disabled={loading} type="submit" className="btn btn-info text-white">
+            <div className="text-end mt-3">
+              <CustomTable.ActionButtonTemplate
+                variant="success"
+                disabled={loading || !newTaxOptions.length}
+                type="submit"
+              >
                 {loading ? (
                   <>
                     <span className="spinner-grow spinner-grow-sm"></span>
-                    <span className="d-inline-block ms-2">Updating..</span>
+                    <span className="d-inline-block ms-2">Saving..</span>
                   </>
                 ) : (
-                  "Update"
+                  "Save"
                 )}
                 <i className="trailing-icon material-icons">save</i>
-              </button>
+              </CustomTable.ActionButtonTemplate>
             </div>
           </div>
         </div>
@@ -180,4 +161,4 @@ export default function VatSettings({}) {
   );
 }
 
-VatSettings.layout = (page) => <Layout children={page} title={"Sales Categories"} />;
+VatSettings.layout = (page) => <Layout children={page} title={"Tax Options"} />;
