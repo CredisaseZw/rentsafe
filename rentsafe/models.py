d2ff9ee0ef286d867104d561c575b512f4df5275
sehmaluva <machinguramalvint@gmail.com>
2025-06-30 16:31:47 +0200

    Change ForeignKey on LeaseAddress to set null on delete

--- rentsafe/models.py
+++ rentsafe/models.py
@@ -249,7 +249,7 @@ class Lease(models.Model):
         return str(self.lease_id)
 
 class LeaseAddress(models.Model):
-    lease_id = models.ForeignKey(Lease, on_delete=models.PROTECT, max_length=255, related_name="lease_address", blank=True, null=True)
+    lease_id = models.ForeignKey(Lease, on_delete=models.SET_NULL, max_length=255, related_name="lease_address", blank=True, null=True)
     unit_number = models.CharField(max_length=255, blank=True, null=True)
     building_name = models.CharField(max_length=255, blank=True, null=True)
     street_number = models.CharField(max_length=255)
