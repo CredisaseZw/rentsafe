e636b936f2d5fee6c52b6819f91429b7d3c37dd0
Russellg <dudurussell@gmail.com>
2025-07-07 16:07:44 +0200

    feat: define navlinksToRoutes function

--- new-frontend/src/lib/utils.ts
+++ new-frontend/src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import EmptyComponent from "@/components/general/EmptyComponent";
+import type { NavLink, Route } from "@/types";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+   return twMerge(clsx(inputs));
+}
+
+export function navlinksToRoutes(navLinks: NavLink[]): Route[] {
+   return navLinks
+      .map((navLink) => {
+         if (navLink.subLinks) {
+            const routes = navLink.subLinks.map((subLink) => navlinksToRoutes([subLink])).flat();
+            return routes;
+         }
+         return {
+            label: navLink.label,
+            path: navLink.path,
+            component: navLink._component || EmptyComponent,
+         } as Route;
+      })
+      .flat();
 }
