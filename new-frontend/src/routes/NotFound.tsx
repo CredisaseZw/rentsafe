4c7a5de3227257ed5cfacdb8db72dd18cd6225b1
Russellg <dudurussell@gmail.com>
2025-07-07 12:23:51 +0200

    feat: add home link to catch-all route

--- new-frontend/src/routes/NotFound.tsx
+++ new-frontend/src/routes/NotFound.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { useLocation, useNavigate } from "react-router";
+import { ArrowLeft, Home } from "lucide-react";
+import { Link, useLocation, useNavigate } from "react-router";
 
 export default function NotFound() {
    const { pathname } = useLocation();
@@ -14,8 +15,17 @@ export default function NotFound() {
 
          <p className="mt-4 text-center">This page is either still in development or does not exist.</p>
 
-         <div className="mt-8 flex justify-center">
-            <Button onClick={goBack}>Go Back</Button>
+         <div className="mt-8 flex justify-center gap-2">
+            <Button onClick={goBack}>
+               <ArrowLeft />
+               Go Back
+            </Button>
+
+            <Button variant="secondary" size="icon" asChild>
+               <Link to="/">
+                  <Home />
+               </Link>
+            </Button>
          </div>
       </div>
    );
