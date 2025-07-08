2a451ee4893f1240257b9869076327b4844ef1b3
Russellg <dudurussell@gmail.com>
2025-07-08 12:59:33 +0200

    feat: add Logo component

---
+++ new-frontend/src/components/general/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+   return (
+      <h1 className="flex items-center justify-center gap-2 text-3xl">
+         <img src="/logo.svg" className="w-5" />
+         Credisafe
+      </h1>
+   );
+}
