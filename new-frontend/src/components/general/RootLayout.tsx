aa808c540e184739b7e231fed39cc72e539a01c3
Russellg <dudurussell@gmail.com>
2025-07-08 12:24:47 +0200

    feat: tweak max-width of main container

--- new-frontend/src/components/general/RootLayout.tsx
+++ new-frontend/src/components/general/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function RootLayout() {
    return (
       <div>
-         <main className="mx-auto max-w-7xl p-2">
+         <main className="mx-auto max-w-[1440px] p-2">
             <Outlet />
          </main>
       </div>
