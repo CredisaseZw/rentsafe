f4d0507aa9b27620d9aed2db64ee3efc4f73fee1
Russellg <dudurussell@gmail.com>
2025-07-09 15:34:42 +0200

    feat: setup dashboard tabs

---
+++ frontend/src/components/routes/rent-safe/dashboard/IndividualPaymentStatusTab.tsx
@@ -0,0 +1,3 @@
+export default function IndividualPaymentStatusTab() {
+   return <div>Individual Payment Status Check</div>;
+}
--- frontend/src/routes/rent-safe/Dashboard.tsx
+++ frontend/src/routes/rent-safe/Dashboard.tsx
@@ -1,3 +1,34 @@
+import CompanyPaymentStatusTab from "@/components/routes/rent-safe/dashboard/CompanyPaymentStatusTab";
+import OverviewTab from "@/components/routes/rent-safe/dashboard/OverviewTab";
+import IndividualPaymentStatusTab from "@/components/routes/rent-safe/dashboard/IndividualPaymentStatusTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function Dashboard() {
-   return <div>Dashboard</div>;
+   const tabs = [
+      { value: "overview", label: "Overview", content: <OverviewTab /> },
+      {
+         value: "individual-payment-status",
+         label: "Individual Payment Status",
+         content: <IndividualPaymentStatusTab />,
+      },
+      { value: "company-payment-status", label: "Company Payment Status", content: <CompanyPaymentStatusTab /> },
+   ];
+
+   return (
+      <Tabs defaultValue={tabs[0].value} className="w-[400px]">
+         <TabsList>
+            {tabs.map((tab) => (
+               <TabsTrigger key={tab.value} value={tab.value}>
+                  {tab.label}
+               </TabsTrigger>
+            ))}
+         </TabsList>
+
+         {tabs.map((tab) => (
+            <TabsContent key={tab.value} value={tab.value}>
+               {tab.content}
+            </TabsContent>
+         ))}
+      </Tabs>
+   );
 }
