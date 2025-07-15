629f6aaeb1868d0c6b2912e3bc79d4b2ac9f7249
Russellg <dudurussell@gmail.com>
2025-07-15 14:23:28 +0200

    feat: make logo customizable

--- frontend/src/components/general/Logo.tsx
+++ frontend/src/components/general/Logo.tsx
@@ -1,7 +1,14 @@
-export default function Logo() {
+import { cn } from "@/lib/utils";
+
+type LogoProps = {
+   className?: string;
+   imageClassName?: string;
+};
+
+export default function Logo({ className, imageClassName }: LogoProps) {
    return (
-      <h1 className="flex items-center justify-center gap-2 text-3xl">
-         <img src="/logo.svg" className="w-5" />
+      <h1 className={cn("flex items-center justify-center gap-2 text-3xl", className)}>
+         <img src="/logo.svg" className={cn("w-5", imageClassName)} />
          Credisafe
       </h1>
    );
