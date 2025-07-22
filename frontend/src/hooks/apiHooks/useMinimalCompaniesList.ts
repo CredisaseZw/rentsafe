ed0054cb3702ddb3f2f11d551200b3d3bf004e7c
Russellg <dudurussell@gmail.com>
2025-07-22 10:54:30 +0200

    refactor, feat: seperate Company type to CompanyMinimal and CompanyFull

--- frontend/src/hooks/apiHooks/useMinimalCompaniesList.ts
+++ frontend/src/hooks/apiHooks/useMinimalCompaniesList.ts
@@ -1,12 +1,12 @@
 import React from "react";
-import type { Company } from "@/interfaces";
+import type { CompanyMinimal } from "@/interfaces";
 import { useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { api } from "@/api/axios";
 
 export default function useMinimalCompaniesList() {
    const { data, isLoading, error } = useQuery<{
-      data: { count: number; next?: string; previous?: string; results: Company[] };
+      data: { count: number; next?: string; previous?: string; results: CompanyMinimal[] };
    }>({
       queryKey: ["minimal-companies-list"],
       queryFn: async () => await api.get("/api/companies/"),
