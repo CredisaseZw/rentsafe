7e86b3f524d9c89820a7bf6a5e03be7c217209f3
godfrey-tankan <godfreytantswakandeya@gmail.com>
2025-09-09 10:25:58 +0200

    feat: Update Dockerfile entrypoint to include gunicorn and modify settings for static and media files

--- core/settings.py
+++ core/settings.py
@@ -241,9 +241,6 @@ CORS_ALLOW_METHODS = [
     'OPTIONS',
 ]
 
-
-
-
 # CRON JOBS
 # https://django-cron.readthedocs.io/en/latest/installation.html#installation
 CRON_CLASSES = [
@@ -563,7 +560,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = "login"
 LOGOUT_REDIRECT_URL = "/"
 
