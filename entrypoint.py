f88597807e49cb74994480cce30b05a815689ae2
CredisafeZW <credisafezw@gmail.com>
2025-09-18 16:07:33 +0200

    fix: Update superuser creation logic to check for email instead of username

--- entrypoint.py
+++ entrypoint.py
@@ -36,7 +36,7 @@ def create_superuser():
         from django.contrib.auth import get_user_model
         User = get_user_model()
         
-        if not User.objects.filter(username='admin').exists():
+        if not User.objects.filter(email='admin@admin.com').exists():
             User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
             print("Superuser created.")
         else:
