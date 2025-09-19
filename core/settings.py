83697978aa79864188f9bdb877418712dd9f0af0
godfrey-tankan <godfreytantswakandeya@gmail.com>
2025-09-19 15:51:28 +0200

    fix: Update Celery broker and result backend environment variable handling

--- core/settings.py
+++ core/settings.py
@@ -333,8 +333,8 @@ REDIS_CACHE_LOCATION = "redis://127.0.0.1:6379/1" if DEVELOPMENT else os.getenv(
 # }
 # Celery Configuration
 
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
