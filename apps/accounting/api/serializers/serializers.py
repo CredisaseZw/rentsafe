82ecfd625b2f08bcab5bda7af16635d717091c35
Malvin T Machingura <sehmaluva@gmail.com>
2025-11-21 12:13:03 +0200

    Remove debug print in create method
    Removed debug print statement from create method.

--- apps/accounting/api/serializers/serializers.py
+++ apps/accounting/api/serializers/serializers.py
@@ -78,7 +78,6 @@ class BaseCompanySerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         request = self.context.get("request")
         if request and hasattr(request, "user") and request.user.is_authenticated:
-            print("chilling in the main seriaizers like a pro", request.user)
             validated_data["created_by"] = request.user
         return super().create(validated_data)
 
